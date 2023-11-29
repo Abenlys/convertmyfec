1a9f003157fb524a0f2ba871d6a407b40e1435d6
Thomas Ramillon <zerglavisse@gmail.com>
2023-11-29 16:24:16 +0100

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,6 +3,7 @@ import styles from "./footer.module.css";
 import Image from 'next/image';
 import linkedin from "public/linkedin.svg"
 import Link from 'next/link';
+import mail from "public/mail.svg"
 
 export default function Footer() {
   return (
@@ -10,6 +11,9 @@ export default function Footer() {
         <p className={styles.footertext}>Developed by TomRam ©2023 – All Rights Reserved</p>
         <Link target='blank' href={"https://www.linkedin.com/in/thomas-ramillon-0b584611b"}>
         <Image width={30} height={30} src={linkedin} alt='linkedin' />
+        </Link>
+        <Link href={"mailto:thomas.ramillon@gmail.com"}>
+        <Image width={30} height={30} src={mail} alt='mail' />
         </Link>
     </div>
   )
